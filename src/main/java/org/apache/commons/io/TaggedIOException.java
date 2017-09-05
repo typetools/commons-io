f134fb36636a715aad1430203e71bac1de43d4f9
nikshinde1996 <crenickshinde1996@gmail.com>
2017-09-05 20:15:24 +0530

    Suppress false +ve warning in TaggedIOException class.

--- src/main/java/org/apache/commons/io/TaggedIOException.java
+++ src/main/java/org/apache/commons/io/TaggedIOException.java
@@ -93,6 +93,9 @@ public class TaggedIOException extends IOExceptionWithCause {
      * @param tag tag object
      * @throws IOException original exception from the tagged decorator, if any
      */
+    @SuppressWarnings("throwing.nullable")
+    // For TaggedIOException, cause of Exception is non-null. In constructor of TaggedIOException class
+    // non-null value for cause is set using superclass constructor.
     public static void throwCauseIfTaggedWith(final Throwable throwable, final Object tag)
             throws IOException {
         if (isTaggedWith(throwable, tag)) {
